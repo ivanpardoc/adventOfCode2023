6919b4f7df8bf91047b3e41e1a409c97f3221915
Ivan Pardo <ivan@thesustainables.nl>
2023-12-04 08:51:50 +0100

    Day 4 part 1

--- day4/day4Part2.js
+++ day4/day4Part2.js
@@ -2,19 +2,19 @@ import { input, example, exampleCustom } from './input.js';
 const total = [];
 
 function solveDay4() {
-    const inputData = input;
+    const inputData = example;
 
-    inputData.forEach(card => {
+    inputData.forEach((card, indexCard) => {
         let winningNumbers = card.split(' | ')[0];
         let myNumbers = card.split(' | ')[1];
         const cardId = winningNumbers.split(': ')[0];
         winningNumbers = winningNumbers.split(': ')[1].split(' ');
         myNumbers = myNumbers.split(' ');
         const matchingNumbers = myNumbers.filter((num) => winningNumbers.includes(num) && num !== '');
-        let value = 0;
-        matchingNumbers.forEach((match) => {
-            value = value === 0 ? 1 : value * 2;
+        matchingNumbers.forEach((element, indexW) => {
+            inputData.splice(indexCard + indexW + 1);
         });
+        console.log(inputData);
         total.push(value);
     });
 
