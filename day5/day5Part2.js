b003230a10509a397053c58d72d8d092205ff8a7
Ivan Pardo <ivan@thesustainables.nl>
2023-12-05 11:11:21 +0100

    Day 5 part 2

--- day5/day5Part2.js
+++ day5/day5Part2.js
@@ -8,10 +8,12 @@ let waterToLightMapping = [];
 let lightToTemperatureMapping = [];
 let temperatureToHumidityMapping = [];
 let humidityToLocationMapping = [];
+let seedsRanges = [];
 
 function solveDay5() {
-    const { seeds, seedToSoil, soilToFertilizer, fertilizerToWater, waterToLight, lightToTemperature, temperatureToHumidity, humidityToLocation} = inputExample;
+    const { seeds, seedToSoil, soilToFertilizer, fertilizerToWater, waterToLight, lightToTemperature, temperatureToHumidity, humidityToLocation} = input;
     const seedsSplitted = seeds.split(' ');
+    // getAllSeeds(seedsSplitted);
     seedsToSoilMapping = mapCoordinates(seedToSoil);
     soilToFertilizerMapping = mapCoordinates(soilToFertilizer);
     fertilizerToWaterMapping = mapCoordinates(fertilizerToWater);
@@ -19,29 +21,13 @@ function solveDay5() {
     lightToTemperatureMapping = mapCoordinates(lightToTemperature);
     temperatureToHumidityMapping = mapCoordinates(temperatureToHumidity);
     humidityToLocationMapping = mapCoordinates(humidityToLocation);
-
-    seedsSplitted.forEach((seed) => {
-        // const seed = '79';
-        const soil = getMapping(parseInt(seed), seedsToSoilMapping);
-        const fertilizer = getMapping(soil, soilToFertilizerMapping);
-        const water = getMapping(fertilizer, fertilizerToWaterMapping);
-        const light = getMapping(water, waterToLightMapping);
-        const temperature = getMapping(light, lightToTemperatureMapping);
-        const humidity = getMapping(temperature, temperatureToHumidityMapping);
-        const location = getMapping(humidity, humidityToLocationMapping);
-        lowestNumber = lowestNumber > location || !lowestNumber ? location : lowestNumber
-    });
-    
-    
-    console.log('lowestNumber', lowestNumber);
-
-
+    getAllSeeds(seedsSplitted);
 }
 
 function getMapping(numberToCheck, arr) {
     let numberFound = 0;
     arr.forEach(item => {
-        if (numberToCheck > item.og && numberToCheck < item.og + item.range ) {
+        if (numberToCheck >= item.og && numberToCheck < item.og + item.range ) {
             return numberFound = numberToCheck + (item.dest - item.og);
         } else {
             return numberFound = numberFound === 0 ? numberToCheck : numberFound;
@@ -65,6 +51,30 @@ function mapCoordinates(ogToDest) {
     return mapping;
 }
 
+
+function getAllSeeds(seeds) {
+    const length = seeds.length;
+    for (let index = 0; index < seeds.length; index++) {
+        const seed = parseInt(seeds[index]);
+        if (index === 0 || index%2 === 0) {
+            for (let indext = 0; indext < seeds[index+1]; indext++) {
+                const newseed = seed + (indext);
+                const soil = getMapping(parseInt(newseed), seedsToSoilMapping);
+                const fertilizer = getMapping(soil, soilToFertilizerMapping);
+                const water = getMapping(fertilizer, fertilizerToWaterMapping);
+                const light = getMapping(water, waterToLightMapping);
+                const temperature = getMapping(light, lightToTemperatureMapping);
+                const humidity = getMapping(temperature, temperatureToHumidityMapping);
+                const location = getMapping(humidity, humidityToLocationMapping);
+                lowestNumber = lowestNumber > location || !lowestNumber ? location : lowestNumber
+            }
+            console.log('seed:', seed, 'lowestNumber:', lowestNumber);
+        }
+        
+    }
+}
 solveDay5();
 
-// 177942185 - OK
+// 210388587 - TOO HIGH
+// 120063312 - TOO HIGH
+// 69841803 - Ok
